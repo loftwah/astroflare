50277c174892f249e5e853a4f280349cb76e6fb1
Dean Lofts <dean@deanlofts.xyz>
2025-03-20 16:39:21 +1100

    progress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindPlugin from "@tailwindcss/vite";
 
-import tailwindcss from '@tailwindcss/vite';
+import cloudflare from "@astrojs/cloudflare";
 
-// https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindPlugin()],
+  },
+
+  // Enable SSR for Functions
+  output: "server",
+
+  adapter: cloudflare()
 });
